f2b204506f059d008c5d661f1f1c45ccf2174a7f
zidanegimiga <zidanegimiga@gmail.com>
2022-09-22 22:20:23 +0300

    feat: create Hero section

---
+++ src/features/Home/Hero/Hero.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Button from 'shared/Button';
+import Image from 'next/image'
+import styles from './Hero.module.scss'
+
+const Hero = () => {
+  return (
+    <div className={styles.heroContainer}>
+        <div className={styles.heroLeft}>
+          <h1 className={styles.title}>Find <span>Real Estate Property</span><br/> to Buy, Sell, or Rent</h1>
+          <h2 className={styles.subHeading}>Bricks & Acres is an upcoming property investment platform that allows users to discover real estate property with much ease.</h2>
+          <p>Want to be notified when the product launches?</p>
+          <Button text="NOTIFY ME" variant="animated" />
+        </div>
+        <div className={styles.heroRight}>
+          <Image src="/hero.png" alt="hero image" width={680} height={600}/>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
