c5e3dfb82afbb6bea9ab271913f953164a409b3c
zidanegimiga <zidanegimiga@gmail.com>
2022-09-22 16:03:22 +0300

    feat: create button component

---
+++ src/shared/Button/Button.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from './Button.module.scss'
+
+export default function Button ({text, variant}){
+    if (variant === "fill"){
+        return (
+            <>
+              <div className={styles.ButtonContainerFill}>
+                {text}
+              </div>
+            </>
+        )
+    } else {
+        return(
+            <>
+              <div className={styles.ButtonContainerOutline}>
+                {text}
+              </div>
+            </>
+        )
+    }
+}
