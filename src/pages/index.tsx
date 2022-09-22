698e13796356a528f9c3537ffdffa17b105baa7a
zidanegimiga <zidanegimiga@gmail.com>
2022-09-22 23:32:48 +0300

    feat: create what we section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,14 @@ import Link from 'next/link';
 import React from 'react';
 import Hero from 'features/Home/Hero/';
 import WhoWeAre from 'features/Home/WhoWeAre';
+import WhatWeDo from 'features/Home/WhatWeDo';
 
 export default function Index() {
  return(
   <>
     <Hero />
     <WhoWeAre />    
+    <WhatWeDo />    
   </>
  )
 }
