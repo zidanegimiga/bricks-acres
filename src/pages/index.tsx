8dcf1ff00da7e388137469294b4e0aad51eac294
zidanegimiga <zidanegimiga@gmail.com>
2022-09-22 22:21:19 +0300

    feat: add created landing page sections

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
 import Link from 'next/link';
 import React from 'react';
+import Hero from 'features/Home/Hero/';
+import WhoWeAre from 'features/Home/WhoWeAre';
 
 export default function Index() {
  return(
-  <>    
+  <>
+    <Hero />
+    <WhoWeAre />    
   </>
  )
 }
