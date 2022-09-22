5b3ad8f0eec1986fd2bf70bffe5bc4b4342dba16
zidanegimiga <zidanegimiga@gmail.com>
2022-09-22 17:57:25 +0300

    feat: add button variant

--- src/shared/Button/Button.tsx
+++ src/shared/Button/Button.tsx
@@ -1,22 +1,25 @@
-import React from 'react';
-import styles from './Button.module.scss'
+import React from "react";
+import styles from "./Button.module.scss";
 
-export default function Button ({text, variant}){
-    if (variant === "fill"){
-        return (
-            <>
-              <div className={styles.ButtonContainerFill}>
-                {text}
-              </div>
-            </>
-        )
-    } else {
-        return(
-            <>
-              <div className={styles.ButtonContainerOutline}>
-                {text}
-              </div>
-            </>
-        )
-    }
+export default function Button({ text, variant }) {
+  if (variant === "fill") {
+    return (
+      <>
+        <div className={styles.ButtonContainerFill}>{text}</div>
+      </>
+    );
+  } else if (variant === "animated") {
+    return (
+      <div className={styles.AnimatedButtonContainer}>
+        <span>{text}</span>
+        <div className={styles.ButtonArrow}></div>
+      </div>
+    );
+  } else {
+    return (
+      <>
+        <div className={styles.ButtonContainerOutline}>{text}</div>
+      </>
+    );
+  }
 }
