3e949892077084d9b1c3cf8542217e79e0396ff7
zidanegimiga <zidanegimiga@gmail.com>
2022-09-23 08:06:05 +0300

    feat: add component to main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,17 @@ import React from 'react';
 import Hero from 'features/Home/Hero/';
 import WhoWeAre from 'features/Home/WhoWeAre';
 import WhatWeDo from 'features/Home/WhatWeDo';
+import WhoWeServe from 'features/Home/WhoWeServe';
+import SignUpForm from 'features/Home/SignUpForm';
 
 export default function Index() {
  return(
   <>
     <Hero />
     <WhoWeAre />    
-    <WhatWeDo />    
+    <WhatWeDo />
+    <WhoWeServe />
+    <SignUpForm />    
   </>
  )
 }
