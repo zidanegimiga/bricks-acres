f60f8200647c0b1b7eb5bae34c6830e8d5b4c20c
zidanegimiga <zidanegimiga@gmail.com>
2022-09-25 08:56:56 +0300

    feat: create footer

--- src/shared/Footer/Footer.tsx
+++ src/shared/Footer/Footer.tsx
@@ -1,8 +1,53 @@
 import React from 'react'
+import Image from "next/image"
+import Link from 'next/link'
+import styles from './Footer.module.scss'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className={styles.sectionContainer}>
+      <div className={styles.footerContainer}>
+        <div className={styles.contentWrapper}>
+          <div className={styles.footerLogo}>
+            <Image alt="Bricks and Acres Logo" width={200} height={190} src="/logo-bg-dark.png"/>
+          </div>
+          <div className={styles.footerContacts}>
+            <h3 className={styles.sectionTitle}>CONNECT WITH US</h3>
+            <Link href="/contact"> 
+              <p className={styles.contactUs}>Contact us</p>
+            </Link>
+            <p className={styles.contact}>info@bricksandacres.io</p>
+            <p className={styles.contact}>Western Heights, 10th Floor,<br/>Karuna Road, Westlands, Nairobi.</p>
+          </div>
+          <div className={styles.footerSocials}>
+            <h3 className={styles.sectionTitle}>FOLLOW US</h3>
+            <div className={styles.socialButtons}>
+              <div className={styles.socialButton}>
+                <Image src="/facebook.png" width={32} height={32} alt="facebook icon"/>
+              </div>
+              <div className={styles.socialButton}>
+                <Image src="/whatsapp.png" width={32} height={32} alt="whatsapp icon"/>
+              </div>
+              <div className={styles.socialButton}>
+                <Image src="/instagram.png" width={32} height={32} alt="instagram icon"/>
+              </div>
+            </div>        
+          </div>
+          <div className={styles.scrollToTopContainer}>
+            <div className={styles.arrowUpContainer}>
+              <Image src="/arrow-up.png" alt="Arrow up icon" width={32} height={32}/>
+            </div>
+            <p>Scroll to top</p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.footerIllustration}>
+        <Image src="/footerIllustration.png" width={2000} height={400} alt="footer illustration"/>        
+      </div>
+      <div className={styles.copyright}>
+        <p>© 2022 Bricks and Acres | All rights reserved</p>
+      </div>
+    </div>
   )
 }
 
