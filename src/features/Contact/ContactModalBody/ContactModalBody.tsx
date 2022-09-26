a0b40db2e713e22d7a1970f24713d39e9f86ff5f
zidanegimiga <zidanegimiga@gmail.com>
2022-09-26 10:04:07 +0300

    feat: create  content for Contact modal

---
+++ src/features/Contact/ContactModalBody/ContactModalBody.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import Image from 'next/image'
+import styles from "./ContactModalBody.module.scss"
+
+const ContactModalBody = () => {
+  return (
+    <>
+      <div className={styles.modalHeader}>
+        <div className={styles.logo}>
+          <Image alt="logo" width={80} height={80} src="/logo.png" />
+        </div>
+      </div>
+      <div className={styles.modalContentBody}>
+        <h1>
+          Thank you
+          <br /> for Contacting us
+        </h1>
+      </div>
+      <div className={styles.modalFooter}>
+        <h3>Engage with us</h3>
+        <div className={styles.titleUnderline}></div>
+        <div className={styles.socialIcons}>
+          <div className={styles.socialButton}>
+            <Image
+              src="/facebookDark.svg"
+              width={48}
+              height={48}
+              alt="facebook icon"
+            />
+          </div>
+          <div className={styles.socialButton}>
+            <Image
+              src="/whatsappDark.svg"
+              width={48}
+              height={48}
+              alt="whatsapp icon"
+            />
+          </div>
+          <div className={styles.socialButton}>
+            <Image
+              src="/instagramDark.svg"
+              width={48}
+              height={48}
+              alt="instagram icon"
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ContactModalBody;
