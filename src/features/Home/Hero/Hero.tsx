cbc1e350d6713afbcd47c7184058e37d0d69d110
zidanegimiga <zidanegimiga@gmail.com>
2022-10-05 16:41:58 +0300

    style: make hero page responsive

--- src/features/Home/Hero/Hero.tsx
+++ src/features/Home/Hero/Hero.tsx
@@ -14,8 +14,9 @@ const Hero = () => {
           <Button text="Notify me" variant="animated" route="/#signUp"/>
         </div>
         <div className={styles.heroRight}>
-          <Image src="/hero.png" alt="hero image" width={680} height={600}/>
+          <Image src="/hero.png" alt="hero image" width={720} height={720}/>
         </div>
+        <div className={styles.overlay}></div>
     </div>
   )
 }
